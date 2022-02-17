c46cb04d0f34c1ff9d3fe393b0ae19d8edb55b00
Md Naim Hossain Arif <thk.naim@gmail.com>
2022-02-17 18:13:19 +0100

    Initial commit

---
+++ src/com/myHotelPortal/Customers.java
@@ -0,0 +1,8 @@
+package com.myHotelPortal;
+
+public class Customers {
+    private String customerName;
+    private String customerAddress;
+    private String customerEmail;
+    private double customerPhone;
+}
---
+++ src/com/myHotelPortal/Hotels.java
@@ -0,0 +1,60 @@
+package com.myHotelPortal;
+
+public class Hotels {
+    private String hotelName;
+    private String hotelLocation;
+    private int totalRooms;
+    private int totalSingleRooms;
+    private int totalDoubleRooms;
+    private int totalPresidentialSuit;
+
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getHotelLocation() {
+        return hotelLocation;
+    }
+
+    public void setHotelLocation(String hotelLocation) {
+        this.hotelLocation = hotelLocation;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+
+    public int getTotalSingleRooms() {
+        return totalSingleRooms;
+    }
+
+    public void setTotalSingleRooms(int totalSingleRooms) {
+        this.totalSingleRooms = totalSingleRooms;
+    }
+
+    public int getTotalDoubleRooms() {
+        return totalDoubleRooms;
+    }
+
+    public void setTotalDoubleRooms(int totalDoubleRooms) {
+        this.totalDoubleRooms = totalDoubleRooms;
+    }
+
+    public int getTotalPresidentialSuit() {
+        return totalPresidentialSuit;
+    }
+
+    public void setTotalPresidentialSuit(int totalPresidentialSuit) {
+        this.totalPresidentialSuit = totalPresidentialSuit;
+    }
+
+}
